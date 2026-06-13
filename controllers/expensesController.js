a30920a202684b8b5ed6414ec1e8bb4977dfd426
Pavan Sai <kavuripavansai94@gmalil.com>
2026-06-13 15:39:11 +0530

    Fix supabase import in expenses controller

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -1,4 +1,4 @@
-const supabase = require('../config/supabase');
+const { supabase } = require('../config/supabase');
 
 // Helper to generate recurring expenses for the current month
 const checkAndGenerateRecurringExpenses = async () => {
